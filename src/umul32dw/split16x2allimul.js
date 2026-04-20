ee0cf13bb8a999c98f024eb66f592d0d8c394b1e
Abdul Kaium <abdulkaium1024@gmail.com>
2026-04-21 01:09:42 +0600

    Add all imul and all imul signed variant for 16x2

---
+++ src/umul32dw/split16x2allimul.js
@@ -0,0 +1,31 @@
+"use strict";
+
+import { LOW_16 } from "./constants.js";
+
+// 64-bit result of multiplying two 32-bit uint
+// Using Math.imul for every multiplication in the split16x2 flow
+export default function umul32dw(a, b, out) {
+  a >>>= 0;
+  b >>>= 0;
+
+  const ah = a >>> 16;
+  const al = a & LOW_16;
+  const bh = b >>> 16;
+  const bl = b & LOW_16;
+
+  const albl = Math.imul(al, bl) >>> 0;
+  const llh = albl >>> 16;
+
+  const ahbl = (Math.imul(ah, bl) + llh) >>> 0;
+  const hll = ahbl & LOW_16;
+  const hlh = ahbl >>> 16;
+
+  const albh = (Math.imul(al, bh) + hll) >>> 0;
+  const lhh = albh >>> 16;
+
+  const lo = Math.imul(a, b) >>> 0;
+  const hi = (Math.imul(ah, bh) + hlh + lhh) >>> 0;
+
+  out[0] = hi;
+  out[1] = lo;
+}
