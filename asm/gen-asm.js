8851327d537bcd2863af7b6491a6b58e5812800b
Abdul Kaium <abdulkaium1024@gmail.com>
2026-04-21 05:35:11 +0600

    Add local constant variants for umul32dw

--- asm/gen-asm.js
+++ asm/gen-asm.js
@@ -1,10 +1,15 @@
 "use strict";
 
 import f16x1 from '#src/umul32dw/split16x1.js';
-import f16x2 from '#src/umul32dw/split16x2.js';
+import f16x1c from '#src/umul32dw/split16x1c.js';
 import f16x1i from '#src/umul32dw/split16x1imul.js';
+import f16x1ic from '#src/umul32dw/split16x1imulc.js';
+import f16x2 from '#src/umul32dw/split16x2.js';
+import f16x2c from '#src/umul32dw/split16x2c.js';
 import f16x2i from '#src/umul32dw/split16x2imul.js';
+import f16x2ic from '#src/umul32dw/split16x2imulc.js';
 import f16x2ai from '#src/umul32dw/split16x2allimul.js';
+import f16x2aic from '#src/umul32dw/split16x2allimulc.js';
 import f16x2ais from '#src/umul32dw/split16x2ais.js';
 
 import process from "node:process";
@@ -13,10 +18,15 @@ const name = process.argv[2];
 
 const funcs = {
   '16x1': f16x1,
-  '16x2': f16x2,
+  '16x1c': f16x1c,
   '16x1i': f16x1i,
+  '16x1ic': f16x1ic,
+  '16x2': f16x2,
+  '16x2c': f16x2c,
   '16x2i': f16x2i,
+  '16x2ic': f16x2ic,
   '16x2ai': f16x2ai,
+  '16x2aic': f16x2aic,
   '16x2ais': f16x2ais,
 }
 
@@ -32,7 +42,7 @@ function optimize(f) {
   const res = new Uint32Array(2);
 
   // Collect type information on next call
-  %PrepareFunctionForOptimization(f)
+  % PrepareFunctionForOptimization(f)
 
   // Call function once to fill type information
   f(0xffffffff, 0xffffffff, res);
@@ -41,7 +51,7 @@ function optimize(f) {
   f(0xdeadbeef, 0x8badf00d, res);
 
   // Force TurboFan compilation on next call
-  %OptimizeFunctionOnNextCall(f);
+  % OptimizeFunctionOnNextCall(f);
   f(0xac1dba5e, 0x5a171337, res);
 
   console.log(res);
---
+++ src/umul32dw/split16x1c.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const LOW_16 = 0xffff;
+const TWO_16 = 0x10000;
+
+// 64-bit result of multiplying two 32-bit uint
+export default function umul32dw(a, b, out) {
+  a >>>= 0;
+  b >>>= 0;
+
+  const ah = a >>> 16;
+  const al = a & LOW_16;
+
+  const ahb = ah * b;
+  const alb = al * b;
+
+  const lo = (((ahb & LOW_16) << 16) + (alb >>> 0)) >>> 0;
+  const hi = ((ahb + ((alb / TWO_16) >>> 0)) / TWO_16) >>> 0;
+
+  out[0] = hi;
+  out[1] = lo;
+}
